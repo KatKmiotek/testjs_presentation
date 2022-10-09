060eb31fd25e89b2c754be875d8466acc5e454ae
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2022-10-09 19:59:12 +0100

    edits

--- examples/test.js
+++ examples/test.js
@@ -3,9 +3,51 @@ const { expect } = require('@playwright/test')
 const { EmailPage } = require('../pom/email-page');
 
 test('User can verify email', async ({ emailAPI, page }) => {
+  // pre-condition 
+
+  const HTMLBody = await emailAPI.getEmailBody('testUser@example.com');
+
   const emailPage = new EmailPage(page);
-  const HTML = await emailAPI.getEmailBody('testUser@example.com');
-  await emailPage.goTo(HTML);
+
+  await emailPage.goTo(HTMLBody);
   await emailPage.clickButton();
+
   expect(page.url()).toEqual('https://testjssummit.com/')
 });
+
+test('Visual comparison of email', async ({ emailAPI, page }) => {
+    // pre-condition 
+  
+    const HTMLBody = await emailAPI.getEmailBody('testUser@example.com');
+  
+    const emailPage = new EmailPage(page);
+  
+    await emailPage.goTo(HTMLBody);
+    
+    await expect(page).toHaveScreenshot('email.png');
+  });
+
+  test('Get a button href', async ({ emailAPI, page }) => {
+    // pre-condition 
+  
+    const HTMLBody = await emailAPI.getEmailBody('testUser@example.com');
+  
+    const emailPage = new EmailPage(page);
+    await emailPage.goTo(HTMLBody);
+
+  
+    await expect(emailPage.verifyButton)
+                .toHaveAttribute('href', 'https://testjssummit.com/');
+    
+  });
+test('User can verify email', async () => {
+    // pre-condition i.e user creates the account - email gets sent
+    
+    // want to access SMTP server
+    // get last email that has been sent to the user
+    // get its HTML body part
+    // render page with email content
+    // click verify button 
+    // confirm new tab opens and has expected URL
+
+  });
